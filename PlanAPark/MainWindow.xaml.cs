d6347f740527dbb7c11f03138d4c4d02b60cd5d5
Patrick Boyer <boyer.patrick@gmail.com>
2020-01-12 17:06:13 -0500

    Proportions for the first four airplaines

--- PlanAPark/MainWindow.xaml.cs
+++ PlanAPark/MainWindow.xaml.cs
@@ -10,20 +10,86 @@ namespace PlanAPark
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double MetersToPixelsScale = 20d / 28d;
+
+        public double cessnaLengthInMeters = 8.28d * 4;
+        public double cessnaWidthInMeters = 11d * 4;
+        public double lancasterWidthInMeters = 21.18d * 4;
+        public double lancasterLengthInMeters = 31.09d * 4;
+        public double hornetLengthInMeters = 17.1d * 4;
+        public double hornetWidthInMeters = 12.3d * 4;
+        public double tutorLengthInMeters = 9.75d * 4;
+        public double tutorWidthInMeters = 11.07d * 4;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        private void ImgCessnaImageButton_Click(object sender, RoutedEventArgs e)
         {
             Button b = sender as Button;
             BitmapSource s = b.Tag as BitmapSource;
             Image i = new Image();
             i.MouseDown += ImageButton_StartDrag;
             i.Source = s;
-            i.Height = 48;
-            i.Width = 48;
+            i.Stretch = System.Windows.Media.Stretch.Fill;
+            i.Height = cessnaLengthInMeters * MetersToPixelsScale;
+            i.Width = cessnaWidthInMeters * MetersToPixelsScale;
+            Random r = new Random();
+            var x = r.Next(0, (int)airportCanvas.ActualWidth);
+            var y = r.Next(0, (int)airportCanvas.ActualHeight);
+            Canvas.SetLeft(i, x);
+            Canvas.SetTop(i, y);
+            airportCanvas.Children.Add(i);
+        }
+
+        private void ImgHornetImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            BitmapSource s = b.Tag as BitmapSource;
+            Image i = new Image();
+            i.Stretch = System.Windows.Media.Stretch.Fill;
+            i.MouseDown += ImageButton_StartDrag;
+            i.Source = s;
+            i.Height = hornetLengthInMeters * MetersToPixelsScale;
+            i.Width = hornetWidthInMeters * MetersToPixelsScale;
+            Random r = new Random();
+            var x = r.Next(0, (int)airportCanvas.ActualWidth);
+            var y = r.Next(0, (int)airportCanvas.ActualHeight);
+            Canvas.SetLeft(i, x);
+            Canvas.SetTop(i, y);
+            airportCanvas.Children.Add(i);
+        }
+
+        private void ImgTutorImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            BitmapSource s = b.Tag as BitmapSource;
+            Image i = new Image();
+            i.MouseDown += ImageButton_StartDrag;
+            i.Source = s;
+            i.Stretch = System.Windows.Media.Stretch.Fill;
+            i.Height = tutorLengthInMeters * MetersToPixelsScale;
+            i.Width = tutorWidthInMeters * MetersToPixelsScale;
+            Random r = new Random();
+            var x = r.Next(0, (int)airportCanvas.ActualWidth);
+            var y = r.Next(0, (int)airportCanvas.ActualHeight);
+            Canvas.SetLeft(i, x);
+            Canvas.SetTop(i, y);
+            airportCanvas.Children.Add(i);
+        }
+
+        private void ImgLancasterImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            BitmapSource s = b.Tag as BitmapSource;
+            Image i = new Image();
+            i.Stretch = System.Windows.Media.Stretch.Fill;
+            i.MouseDown += ImageButton_StartDrag;
+            i.Source = s;
+            i.Height = lancasterLengthInMeters * MetersToPixelsScale;
+            i.Width = lancasterWidthInMeters * MetersToPixelsScale;
             Random r = new Random();
             var x = r.Next(0, (int)airportCanvas.ActualWidth);
             var y = r.Next(0, (int)airportCanvas.ActualHeight);
@@ -34,7 +100,6 @@ namespace PlanAPark
 
         private void ImageButton_StartDrag(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Drag start");
             var d = new DataObject();
             d.SetData(typeof(Image), sender);
             DragDrop.DoDragDrop((DependencyObject)e.Source, d, DragDropEffects.Move);
